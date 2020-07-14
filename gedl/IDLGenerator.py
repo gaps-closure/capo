653dd51b257874391493288f38353a5fa24ccaf3
Terrence Ezrol <tezrol@perspectalabs.com>
2020-07-13 20:16:47 -0400

    cleanup to include schema validation

--- gedl/IDLGenerator.py
+++ gedl/IDLGenerator.py
@@ -1,26 +1,65 @@
 import json
 import sys
+import os
+import os.path
+import jsonschema
 from argparse import ArgumentParser
 
-if len(sys.argv) < 3:
-    print("Missing command line arguments. Usage of IDLGenerator is \"python IDLGenerator.py </path/to/gedl/file> </path/for/idl/file>\"")
-    exit()
-
 def argparser():
+    """ Command Line Argument Parsing"""
     parser = ArgumentParser(description='CLOSURE IDL File Generator')
     parser.add_argument('-g','--gedl', required=True, type=str, help='Input GEDL Filepath')
     parser.add_argument('-o','--ofile', required=True, type=str, help='Output Filepath')
     parser.add_argument('-i','--ipc', required=True, type=str, help='IPC Type (Singlethreaded/Multithreaded)')
+    parser.add_argument('-s', '--schema', required=False, type=str,
+                 default='./GEDLSchema.json',
+                 help='override the location of the of the schema if required')
+    parser.add_argument('-L', '--liberal',help="Liberal mode: disable gedl schema check",
+                 default=False, action='store_true') 
     return parser.parse_args()
 
-#Open the generated GEDL JSON file for parsing and IDL file for generation
-args = argparser()
-with open(args.gedl) as edl_file:
-    with open(args.ofile,"w") as idl_file:
-        
-        #Load in the json data to "gedl" variable
-        gedl = json.load(edl_file)
+def get_gedl_schema(schema_location):
+    """Load the schema json to verify against"""
+    basepath = ""
+    #get the module paths to help find the schema in a relitive to ourself
+    if(len(sys.path) > 1):
+        basepath = sys.path[0]
+    path = os.path.join(basepath,schema_location)
+    
+    if(not os.path.exists(path)):
+        #schema not found relitive to the python enviroment, check a local path
+        path = schema_location
+        if(not os.path.exists(path)):
+        #Unable to get python schema
+        raise(IOError("Unable to fild cle schema (expected at): " + path))
+    
+    #we found the schema load it into ram
+    print("Using GEDL schema: " + path)
+    with open(path,"r",encoding="UTF-8") as schemafile:
+        return(json.loads(schemafile.read()))
 
+def check_jsonschema_version():
+    """validate the json schema version is new enogh to process
+        Draft 7 schemas
+    """
+    if(jsonschema.__version__ < "3.2.0"):
+        raise(ModuleNotFoundError("Newer version of jsonschema module required"
+        " (>= 3.2.0)"))
+
+def validate_gedl(gedl_json, schema_json):
+    """validate the GEDL entry is valid against the shcema"""
+    try:
+        jsonschema.validate(gedl_json,schema)
+    except Exception as e:
+        print("")
+        print("Error parsing GEDL")
+        raise
+    print("")
+    print("GEDL is valid")
+
+def generate_idl(gedl, args):
+    """Generate the output IDL file"""
+    with open(args.ofile,"w") as idl_file:
         #Create NextRPC and Okay structs necessary for singlethreaded style
         idl_file.write("struct NextRPC {\n\tint mux;\n\tint sec;\n\tint typ;\n};");
         idl_file.write("\n\nstruct Okay {\n\tint x;\n};")
@@ -53,3 +92,28 @@ with open(args.gedl) as edl_file:
                 else:
                     idl_file.write("\n\t%s ret;" % (call['return']['type']))
                 idl_file.write("\n};")
+
+def main():
+    """IDL Generator entry point"""
+
+    #Open the generated GEDL JSON file for parsing and IDL file for generation
+    args = argparser()
+    with open(args.gedl) as edl_file:
+        #Load in the json data to "gedl" variable
+        gedl = json.load(edl_file)
+
+        if(not args.liberal):
+            #validate the schema
+            check_jsonschema_version()
+            schema = get_gedl_schema(args.schema)
+            try:
+                jsonschema.validate(gedl,schema)
+            except jsonschema.exceptions.ValidationError as schemaerr:
+                print("")
+                print("Unable to validate GEDL json:\n%s"%(str(schemaerr),))
+                sys.exit(-1)
+        #generate the idl
+        generate_idl(gedl,args)
+
+if(__name__=="__main__"):
+    main()
