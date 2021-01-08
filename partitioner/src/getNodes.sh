982a02bdbcd49b79f9eb3f92fcbd10b137192994
Thomas F. Bowen <tbowen@perspectalabs.com>
2021-01-08 09:16:01 -0500

    have it create the data structure

--- partitioner/src/getNodes.sh
+++ partitioner/src/getNodes.sh
@@ -1,1 +1,7 @@
-grep -v " -> " $1 | grep Node | sed -e "s/^  *//" -e "s/ .*//"
+echo "Vertices = {"
+n=`grep -v " -> " $1 | grep Node | sed -e "s/^  *//" -e "s/ .*//"`
+for i in $n
+do
+	echo "$i,"
+done
+echo "};"
