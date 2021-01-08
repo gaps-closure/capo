3bd74ca2ed8d4c905f947a1ddc8ba04ad2d29243
Thomas F. Bowen <tbowen@perspectalabs.com>
2021-01-08 09:29:41 -0500

    eliminate bad logging message

--- partitioner/src/createStaticVarArray.sh
+++ partitioner/src/createStaticVarArray.sh
@@ -13,6 +13,6 @@ done
 echo "];"
 
 
-grep annotation= TFB.dot |grep "GLOBAL_VALUE:" | grep "dbginfo="| sed -e "s/^  *//" -e "s/ .*dbginfo=\"/,/" -e "s/\".*$//" -e "s/ /:/" -e "s/ /:/" -e "s/ /:/" -e "s/ /:/" -e "s/,.*:/,/" -e "s/,/ /" | grep "True 0" | sed -e "s/ .*$//" -e "s/^/STATICVAR\[/" -e "s/$/]=1;/"
+#grep annotation= TFB.dot |grep "GLOBAL_VALUE:" | grep "dbginfo="| sed -e "s/^  *//" -e "s/ .*dbginfo=\"/,/" -e "s/\".*$//" -e "s/ /:/" -e "s/ /:/" -e "s/ /:/" -e "s/ /:/" -e "s/,.*:/,/" -e "s/,/ /" | grep "True 0" | sed -e "s/ .*$//" -e "s/^/STATICVAR\[/" -e "s/$/]=1;/"
 # the last int is 0 for STATICS
 # TRUE FALSE is FALSE for STATICS
