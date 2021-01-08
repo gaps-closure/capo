53b6e0016777840a4c64b9a81a85e426af199227
Thomas F. Bowen <tbowen@perspectalabs.com>
2021-01-08 09:18:42 -0500

    have it create the data structure

--- partitioner/src/getEnclaveNodes.sh
+++ partitioner/src/getEnclaveNodes.sh
@@ -1,1 +1,5 @@
-grep enclave $1 | sed -e "s/^  *//" -e "s/ .*enclave=\"/,/" -e "s/\".*$//"
+en=`grep enclave $1 | sed -e "s/^  *//" -e "s/ .*enclave=\"/,/" -e "s/\".*$//"`
+for i in $en
+do
+	echo "constraint ENCLAVENODES[$i]=1;"
+done
