c4d7849e64a0019e8be2ad1ed71bcb6cca947706
Thomas F. Bowen <tbowen@perspectalabs.com>
2021-01-11 09:19:02 -0500

    optimize case in which no such edge type

--- partitioner/src/createGLOBALDEPArray.sh
+++ partitioner/src/createGLOBALDEPArray.sh
@@ -3,17 +3,26 @@ v=`grep -v " -> " $1 | grep Node | sed -e "s/^  *//" -e "s/ .*//"`
 echo -n "GLOBAL_DEP = ["
 for v1 in $v
 do
-	echo -n "|"
-	for v2 in $v
-	do
-		grep "${v1} -> ${v2}" $1 | grep GLOBAL_DEP 1>out 2>err
-		if test "${?}" == "0"
-		then
-			echo -n "true,"
-		else
+	grep "${v1} -> " $1 | grep GLOBAL_DEP 1>out 2>err
+	if test "${?}" != "0"
+	then
+		for v2 in $v
+		do
 			echo -n "false,"
-		fi
-	done
+		done
+	else
+		echo -n "|"
+		for v2 in $v
+		do
+			grep "${v1} -> ${v2}" $1 | grep GLOBAL_DEP 1>out 2>err
+			if test "${?}" == "0"
+			then
+				echo -n "true,"
+			else
+				echo -n "false,"
+			fi
+		done
+	fi
 	echo ""
 done
 echo "|];"
--- partitioner/src/createParameterArray.sh
+++ partitioner/src/createParameterArray.sh
@@ -3,16 +3,25 @@ echo -n "PARAMETER = ["
 for v1 in $v
 do
 	echo -n "|"
-	for v2 in $v
-	do
-		grep "${v1} -> ${v2}" $1 | grep PARAMETER 1>out 2>err
-		if test "${?}" == "0"
-		then
-			echo -n "true,"
-		else
+	grep "${v1} -> " $1 | grep PARAMETER 1>out 2>err
+	if test "${?}" != "0"
+	then
+		for v2 in $v
+		do
 			echo -n "false,"
-		fi
-	done
+		done
+	else
+		for v2 in $v
+		do
+			grep "${v1} -> ${v2}" $1 | grep PARAMETER 1>out 2>err
+			if test "${?}" == "0"
+			then
+				echo -n "true,"
+			else
+				echo -n "false,"
+			fi
+		done
+	fi
 	echo ""
 done
 echo "|];"
