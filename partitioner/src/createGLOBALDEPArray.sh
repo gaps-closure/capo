ea22d69f12a71584f37fdbd059db464352fd4c93
Thomas F. Bowen <tbowen@perspectalabs.com>
2021-01-18 10:28:29 -0500

    correct syntax

--- partitioner/src/createGLOBALDEPArray.sh
+++ partitioner/src/createGLOBALDEPArray.sh
@@ -3,6 +3,7 @@ v=`grep -v " -> " $1 | grep Node | sed -e "s/^  *//" -e "s/ .*//"`
 echo -n "GLOBAL_DEP = ["
 for v1 in $v
 do
+	echo -n "|"
 	grep "${v1} -> " $1 | grep GLOBAL_DEP 1>out 2>err
 	if test "${?}" != "0"
 	then
@@ -11,7 +12,6 @@ do
 			echo -n "false,"
 		done
 	else
-		echo -n "|"
 		for v2 in $v
 		do
 			grep "${v1} -> ${v2}" $1 | grep GLOBAL_DEP 1>out 2>err
