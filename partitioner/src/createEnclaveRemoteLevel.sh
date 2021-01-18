76eaee20c0a5438d9014a12a5207c1e587359a7f
Thomas F. Bowen <tbowen@perspectalabs.com>
2021-01-18 10:27:58 -0500

    add myDir

--- partitioner/src/createEnclaveRemoteLevel.sh
+++ partitioner/src/createEnclaveRemoteLevel.sh
@@ -1,3 +1,4 @@
+myDir=`dirname $0`
 v=`grep -v " -> " $1.annotated.dot | grep Node | sed -e "s/^  *//" -e "s/ .*//"`
 t=`grep taint $1.annotated.dot | sed -e "s/^  *//" -e "s/ .*taint=\"/,/" -e "s/\".*$//"`
 e=`grep enclave $1.annotated.dot | sed -e "s/^.*enclave=\"//" -e "s/\".*$//" | sort -u`
@@ -16,7 +17,7 @@ do
 		t=`grep "${v1}" $1.annotated.dot | grep taint | sed -e "s/^.*taint=\"//" -e "s/\".*$//"`
 		for e1 in $e
 		do
-			r=`python3 getRemoteLevel.py $1.clemap.json $t`
+			r=`python3 $myDir/getRemoteLevel.py $1.clemap.json $t`
 			if test "${e1}" == "${r}"
 			then
 				echo -n "true,"
