6c859ac59e033ad01349d5293b516cc32baeb0e2
rbrot249 <76820178+rbrot249@users.noreply.github.com>
2021-07-01 18:58:58 -0400

    Update conflictAnalyzer.sh

--- formal/ontology/conflictAnalyzer.sh
+++ formal/ontology/conflictAnalyzer.sh
@@ -10,7 +10,7 @@ done
 
 llvm-link temp_*.bc -o temp.bc
 
-# opt -load ~/program-dependence-graph/build/libpdg.so -minizinc -zinc-debug  < ~/program-dependence-graph/test/example1.mod.bc
+opt -load ~/program-dependence-graph/build/libpdg.so -minizinc -zinc-debug  < ~/program-dependence-graph/test/example1.mod.bc
 
 minizinc conflict_analyzer.mzn init_cle.mzn pdg_data.dzn conflict_analyzer_decs.mzn cle-data.dzn
 
