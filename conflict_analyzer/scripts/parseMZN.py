69125ba10825952af49e116e198ab4f3fe70cf40
Benjamin Flin <benjamin.flin@peratonlabs.com>
2021-09-01 17:43:51 +0000

    added findmus parsing and generation

--- conflict_analyzer/scripts/parseMZN.py
+++ conflict_analyzer/scripts/parseMZN.py
@@ -6,8 +6,9 @@ import zmq
 import argparse
 import json
 import csv
+import re
 import pandas as pd
-from typing import Optional
+from typing import Any, Dict, Iterable, List, Optional, Set
 from pathlib import Path
 
 
@@ -21,10 +22,8 @@ def main() -> None:
                         help='minizinc output file to process')
     parser.add_argument('--type', '-t', type=str,
                         help='minizinc output file type', choices=['assignment', 'findmus'])
-    parser.add_argument('--node-to-line-number', '-n', type=Path,
-                        required=True, help='Node to line number map')
     parser.add_argument('--pdg-data', '-p', type=Path,
-                        required=('findmus' in sys.argv), help='pdg_data csv')
+                        required=True, help='pdg_data csv')
     parser.add_argument('--zmq', '-z', type=str, nargs='?',
                         help='ZMQ IP Address (tcp://XXX.XXX.XXX.XXX:PORT)')
     parser.add_argument('--output', '-o', type=Path, help='Output file')
@@ -32,141 +31,140 @@ def main() -> None:
     args = parser.parse_args()
     file: Path = args.file
     out_type: str = args.type
-    node_map: Path = args.node_to_line_number
-    pdg_data: Path = args.pdg_data
+    pdg_path: Path = args.pdg_data
     zmq_addr: Optional[str] = args.zmq
-    output: Path = args.output
+    output: Optional[Path] = args.output
 
-    if out_type == 'assignment':
-        result = parseAssignment(file, node_map)
-        topology = result['topology']
-        with open(output, 'w') as of:
-            print(f"Topology written to {output}")
-            of.write(json.dumps(topology, indent=4))
-        if zmq_addr:
-            context = zmq.Context()
-            socket = context.socket(zmq.REQ)
-            socket.connect(args.zmq)
-            socket.send_string(json.dumps(result))
-            print(f"Result sent to {zmq_addr}")
-    elif out_type == 'findmus':
-        conflicts = parseFindMUS(file, pdg_data, node_map)
-        conflicts_ = []
-
-        for c in conflicts:
-            con = {
-                "name": c[2],
-                "description": "TODO",
-                "source": [(c[0], c[1])],
-                "remedies": ["TODO"]
-            }
-            conflicts_.append(con)
-
-        result = {"result": "Conflict", "conflicts": conflicts_}
-        print("Conflict")
-        if zmq_addr:
-            context = zmq.Context()
-            socket = context.socket(zmq.REQ)
-            socket.connect(args.zmq)
-            socket.send_string(json.dumps(result))
-            print(f"Result sent to {zmq_addr}")
+    with open(pdg_path, 'r') as pdg_f:
+        pdg_csv = csv.reader(pdg_f, quotechar='"', skipinitialspace=True)
+        if out_type == 'assignment':
+            result = parseAssignment(file, pdg_csv)
+            topology = result['topology']
+            if output:
+                with open(output, 'w') as of:
+                    print(f"Topology written to {output}")
+                    of.write(json.dumps(topology, indent=4))
+            if zmq_addr:
+                context = zmq.Context()
+                socket = context.socket(zmq.REQ)
+                socket.connect(args.zmq)
+                socket.send_string(json.dumps(result))
+                print(f"Result sent to {zmq_addr}")
+        elif out_type == 'findmus':
+            result = parseFindMUS(file, pdg_csv)
+            conflicts = result['conflicts'] 
+            if zmq_addr:
+                context = zmq.Context()
+                socket = context.socket(zmq.REQ)
+                socket.connect(args.zmq)
+                socket.send_string(json.dumps(result))
+                print(f"Result sent to {zmq_addr}")
+            if output:
+                with open(output, 'w') as of:
+                    print(f"Conflicts written to {output}")
+                    of.write(json.dumps(conflicts, indent=4))
 
 
-def parseAssignment(file: Path, node_map: Path):
-    topology = {}
-    fun_enclave_assingments = []
-    global_enclave_assingments = []
-    fileName = ""
-    print("Success")
-    with open(file) as rf:
-        data = rf.readlines()
-        with open(node_map) as nf:
-            nodes = nf.readlines()
-            for line in data:
-                index = line.find('FunctionEntry ')
-                if index != -1:
-                    sp = line.split(" ")
-                    index = int(sp[5])
-                    enclave = sp[7].split("::")[0].strip("[]")
-                    resStr = ""
-                    for row in nodes:
-                        if int(row.split(",")[0].strip()) == index:
-                            fileName = row.split(",")[-2]
-                            resStr = row.split(
-                                ",")[-2] + ":" + row.split(",")[-1].strip()
-                            funName = row.split(",")[-3].strip()
-                            fun_enclave_assingments.append(
-                                {"name": funName, "level": enclave,  "line": row.split(",")[-1].strip()})
-                            print(
-                                f"Function {funName } on line: {resStr}      Has enclave:  {enclave}")
-                            break
-            for line in data:
-                index = line.find('VarNode ')
-                if index != -1:
-                    sp = line.split(" ")
-                    index = int(sp[5])
-                    enclave = sp[-1].split("::")[0].strip("[]")
-                    resStr = ""
-                    for row in nodes:
-                        if int(row.split(",")[0].strip()) == index:
-                            fileName = row.split(",")[-2]
-                            resStr = row.split(
-                                ",")[-2] + ":" + row.split(",")[-1].strip()
-                            funName = row.split(",")[-3].strip()
-                            global_enclave_assingments.append(
-                                {"name": funName, "level": enclave, "line": row.split(",")[-1].strip()})
-                            print(
-                                f"Function {funName} on line: {resStr}      Has enclave:  {enclave}")
-                            break
-        topology = {"source_path": fileName, "levels": [
-            "ORANGE,PURPLE"], "global_scoped_vars": global_enclave_assingments, "functions": fun_enclave_assingments}
-        result = {"result": "Success", "topology": topology}
-        return result
+def parseAssignment(file: Path, pdg_csv: Iterable) -> Dict[str, Any]:
+    pdg_csv = list(pdg_csv)
+    
+    function_entries : List[Dict[str, str]] = []
+    global_var_entries : List[Dict[str, str]] = []
+    enclaves : Set[str] = set() 
 
+    nodes = sorted(
+        [ (int(num), source, llvm, line) for [type_, num, _, _, llvm, *_, source, line, _]  in pdg_csv if type_ == 'Node' ], 
+        key=lambda x: x[0]
+    )
 
-def parseFindMUS(file: Path, pdg_data_path: Path, node_map: Path):
-    output = []
-    csvfile = open(pdg_data_path)
-    pdg_data = csv.reader(csvfile)
-    edges = []
-    for row in pdg_data:
-        if len(row) > 0:
-            if row[0] == 'Edge':
-                edges.append((row[6], row[7]))
-    print(edges)
-    with open(file, "r") as fileMus:
-        node2LineNumFile = open(node_map)
-        nodeIdx2LineNum = node2LineNumFile.readlines()
-        data = json.loads("".join(fileMus.readlines()[1:-2]))
-        # print (data)
-        constraints = data["constraints"]
-        for constraint in constraints:
-            if constraint["constraint_name"] != "":
-                # print(constraint["assigns"])
-                reason = constraint["constraint_name"]
-                id = constraint["assigns"][2:]
-                if "e=" in constraint["assigns"]:
-                    line1 = nodeIdx2LineNum[int(
-                        edges[int(id)-1][0])-1].split(",")
-                    line2 = nodeIdx2LineNum[int(
-                        edges[int(id)-1][1])-1].split(",")
-                    print(
-                        f"edge: {id} with nodes: {edges[int(id)-1]} failed: {reason}")
-                    print(f"Line 1: {line1} Line 2: {line2}")
-                    output.append(
-                        (line1[-2].strip(), line1[-1].strip(), reason))
-                    output.append(
-                        (line2[-2].strip(), line2[-1].strip(), reason))
-                else:
-                    print(f"node: {id} failed: {reason}")
-                    line = nodeIdx2LineNum[int(id)-1].split(",")
-                    output.append((line[-2].strip(), line[-1].strip(), reason))
+    def add_entry(line: str, entries: List[Dict[str, str]]) -> None:
+        parts = [ s.strip() for s in line.split(" ") if s != '' ]
+        node, [enclave, _label] = int(parts[2]), [ s.strip('[]') for s in parts[-1].split('::') ]
+        node_, source, llvm, line = nodes[node-1]
+        source = None if (stripped := source.strip()) == 'Not Found' else stripped
+        assert node == node_ 
+        match = re.search(r'@((\w|\.)*)', llvm)
+        assert match is not None
+        name = match.group(1)
+        enclaves.add(enclave)
+        entries.append({ "name": name, "level": enclave, "line": line })
+        print(f"{name} is in {enclave}{'' if not source else f' at {source}:{line}'}")
+    
+    with open(file, 'r') as out_f:
+        out = list(out_f) 
+        for line in out:
+            if line.find('FunctionEntry') != -1:
+                add_entry(line, function_entries)
+            elif line.find('VarNode') != -1:
+                add_entry(line, global_var_entries)
+         
+    topology = {
+        "source_path": str(Path('.').resolve()), # provisional, not sure what is correct here
+        "levels": list(enclaves),
+        "global_scoped_vars": global_var_entries,
+        "functions": function_entries
+    }
 
-    for error in output:
-        print(
-            f"In file {error[0]} on line {error[1]}: {error[2]} conflict found ")
+    return { "result": "Success", "topology": topology } 
 
-    return output
+def parseFindMUS(file: Path, pdg_csv: Iterable) -> Dict[str, Any]: 
+    pdg_csv = list(pdg_csv)
+    nodes = sorted(
+        [ (int(num), source, llvm, line) for [type_, num, _, _, llvm, *_, source, line, _]  in pdg_csv if type_ == 'Node' ], 
+        key=lambda x: x[0]
+    )
+    edges = sorted(
+        [ (int(num), int(source), int(dest)) for [type_, num, _, _, _, _, source, dest, *_] in pdg_csv if type_ == 'Edge' ], 
+        key=lambda x: x[0]
+    )
+    for (i, node) in enumerate(nodes):
+        assert i == node[0] - 1
+    for (i, edge) in enumerate(edges):
+        assert i == edge[0] - 1
+
+    with open(file, 'r') as out_f:
+        src = list(out_f)
+        start_index, end_index = None, None
+        for i, line in enumerate(src):
+            if line.find('%%%mzn-json-start') != -1:
+                start_index = i
+            elif line.find('%%%mzn-json-end') != -1:
+                end_index = i
+        assert start_index is not None and end_index is not None
+        json_out = json.loads("\n".join(src[start_index+1:end_index]))
+        conflicts : List[Dict[str, Any]] = []
+        for item in json_out['constraints']:
+            if item['assigns'] and item['assigns'] != '':
+                match = re.search(r'e=(\d+)', item['assigns'])
+                assert match is not None
+                edge_num = int(match.group(1))
+                if edge_num >= len(edges) - 1:
+                    continue
+                _, source, dest = edges[edge_num - 1]
+                (_, source_src, _, source_line), (_, dest_src, _, dest_line) = nodes[source - 1], nodes[dest - 1]
+                conflicts.append({
+                    "name": item['constraint_name'] if item['constraint_name'] != '' else 'Unknown',
+                    "description": "TODO",
+                    "sources": [
+                        { 
+                            "file": source_src, 
+                            "range": { 
+                                "start": { "line": source_line, "character": -1 }, 
+                                "end": { "line": source_line, "character": -1 }, 
+                            },
+                        },                 
+                        {
+                            "file": dest_src,
+                            "range": { 
+                                "start": { "line": dest_line, "character": -1 }, 
+                                "end": { "line": dest_line, "character": -1 }, 
+                            },
+                        }
+                    ],
+                    "remedies": [],
+                })
+        return { "result": "Conflict", "conflicts": conflicts }
+
 
 
 if __name__ == "__main__":
