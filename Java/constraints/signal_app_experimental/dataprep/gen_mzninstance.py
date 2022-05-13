d7a2b1d39129920a73f6dfcf6ce645513bfd6f1a
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2022-05-12 21:12:20 -0400

    clean up scripts for collapsing

--- Java/constraints/signal_app_experimental/dataprep/gen_mzninstance.py
+++ Java/constraints/signal_app_experimental/dataprep/gen_mzninstance.py
@@ -9,11 +9,31 @@ import logging
 from   argparse import ArgumentParser
 from   cle2zinc import compute_zinc
 
-def nparms(x): return 0 if 'p' not in x else len(x['p']) 
+def get_args():
+  p = ArgumentParser(description='Constraint Instance Data Encoder')
+  p.add_argument('-i', '--input_model', required=True, type=str, help='Gzipped Input Program Model JSON file')
+  p.add_argument('-c', '--cle_json', required=True, type=str, help='Input Program Model JSON file')
+  p.add_argument('-o', '--output_dir', required=False, default='./instance', type=str, help='Output Model MZN file')
+  return p.parse_args()
+
+def fix(x): return x.rsplit('.',1)[-1]
+
+def annotsplit(j):
+  for x in j:
+    x['cle-label'] = fix(x['cle-label'])
+    if 'cdf' in x['cle-json']:
+      for y in x['cle-json']['cdf']:
+        if 'rettaints' in y:
+          y['rettaints'] = list(map(fix, y['rettaints']))
+          y['codtaints'] = list(map(fix, y['codtaints']))
+          y['argtaints'] = list(map(lambda w: list(map(fix, w)), y['argtaints']))
+  return j
 
 def brklns(arr,n):
   chunks = [map(str,arr[i:i+n]) for i in range(0, len(arr), n)]
   return '\n'.join([','.join(c)+',' for c in chunks])
+
+def nparms(x): return 0 if 'p' not in x else len(x['p']) 
 
 def esatrack(data):
   if 'classes'   not in data: data['classes'] = []
@@ -103,220 +123,113 @@ class Model():
     if len(self.esamth) > 0: raise('External class method cannot be CLE annotated')
 
     self.allInOrder    = [
-      self.intan,  self.intun,  self.extun, 
-      self.iiafld, self.iiufld, self.eiufld, self.isafld, self.isufld, self.esufld, 
-      self.iiamth, self.iiumth, self.eiumth, self.isamth, self.isumth, self.esumth,
-      self.iicll,  self.iscll,  self.eicll,  self.escll,
-      self.iiacc,  self.isacc,  self.eiacc,  self.esacc
+      (self.intan,  'Class',  'Annotated',   'Internal', ''),
+      (self.intun,  'Class',  'Unannotated', 'Internal', '' ),
+      (self.extun,  'Class',  'Unannotated', 'External', ''),
+      (self.iiafld, 'Field',  'Annotated',   'Internal', 'Instance'),
+      (self.isafld, 'Field',  'Annotated',   'Internal', 'Static'),
+      (self.iiamth, 'Method', 'Annotated',   'Internal', 'Instance'),
+      (self.isamth, 'Method', 'Annotated',   'Internal', 'Static'),
+      (self.iiufld, 'Field',  'Unannotated', 'Internal', 'Instance'),
+      (self.isufld, 'Field',  'Unannotated', 'Internal', 'Static'),
+      (self.iiumth, 'Method', 'Unannotated', 'Internal', 'Instance'),
+      (self.isumth, 'Method', 'Unannotated', 'Internal', 'Static'),
+      (self.eiufld, 'Field',  'Unannotated', 'External', 'Instance'),
+      (self.esufld, 'Field',  'Unannotated', 'External', 'Static'),
+      (self.eiumth, 'Method', 'Unannotated', 'External', 'Instance'),
+      (self.esumth, 'Method', 'Unannotated', 'External', 'Static'),
+      (self.iicll,  'Call',   '',            'Internal', 'Instance'),
+      (self.iscll,  'Call',   '',            'Internal', 'Static'),
+      (self.eicll,  'Call',   '',            'External', 'Instance'),
+      (self.escll,  'Call',   '',            'External', 'Static'),
+      (self.iiacc,  'Access', '',            'Internal', 'Instance'),
+      (self.isacc,  'Access', '',            'Internal', 'Static'),
+      (self.eiacc,  'Access', '',            'External', 'Instance'),
+      (self.esacc,  'Access', '',            'External', 'Static')
     ]
-
-    self.allElements   = [
-      self.iiafld, self.iiufld, self.eiufld, self.isafld, self.isufld, self.esufld, 
-      self.iiamth, self.iiumth, self.eiumth, self.isamth, self.isumth, self.esumth
-    ]
-
-    self.allClasses    = [ self.intan,  self.intun,  self.extun ]
-    self.allFields     = [ self.iiafld, self.iiufld, self.eiufld, self.isafld, self.isufld, self.esufld ]
-    self.allMethods    = [ self.iiamth, self.iiumth, self.eiumth, self.isamth, self.isumth, self.esumth ]
-    self.allCalls      = [ self.iicll, self.iscll, self.eicll, self.escll ]
-    self.allFieldRefs  = [ self.iiacc, self.isacc, self.eiacc, self.esacc ]
-    self.annotElements = [ self.iiafld, self.isafld, self.iiamth, self.isamth ]
 
     # Assign sequential ID to all data items
-    for u,v in enumerate([x for y in self.allInOrder for x in y]): v['q'] = u + 1
+    for u,v in enumerate([x for y,_,_,_,_ in self.allInOrder for x in y]): v['q'] = u + 1
 
-    self.hasClass   = [self.data['classes'][x['c']-1]['q'] for y in self.allElements   for x in y]
-    self.hasCaller  = [self.data['methods'][x['F']-1]['q'] for y in self.allCalls      for x in y]
-    self.hasCallee  = [self.data['methods'][x['T']-1]['q'] for y in self.allCalls      for x in y]
-    self.fromMethod = [self.data['methods'][x['F']-1]['q'] for y in self.allFieldRefs  for x in y]
-    self.toField    = [self.data['fields'][x['T']-1]['q']  for y in self.allFieldRefs  for x in y]
-    self.userAnnElt = [(x['q'], x['G'])                    for y in self.annotElements for x in y]
+    self.allClasses    = [x for x,y,_,_,_ in self.allInOrder if y == 'Class']
+    self.allCalls      = [x for x,y,_,_,_ in self.allInOrder if y == 'Call']
+    self.allFieldRefs  = [x for x,y,_,_,_ in self.allInOrder if y == 'Access']
+    self.allElements   = [x for x,y,_,_,_ in self.allInOrder if y == 'Field' or y == 'Method']
+    self.annotElements = [x for x,y,z,_,_ in self.allInOrder if z == 'Annotated' and (y == 'Field' or y == 'Method')]
+
+    self.hasAnnotation = [x['G'] for y in self.annotElements for x in y]
+    self.hasClass      = [self.data['classes'][x['c']-1]['q'] for y in self.allElements   for x in y]
+    self.callFrom      = [self.data['methods'][x['F']-1]['q'] for y in self.allCalls      for x in y]
+    self.callTo        = [self.data['methods'][x['T']-1]['q'] for y in self.allCalls      for x in y]
+    self.accessFrom    = [self.data['methods'][x['F']-1]['q'] for y in self.allFieldRefs  for x in y]
+    self.accessTo      = [self.data['fields'][x['T']-1]['q']  for y in self.allFieldRefs  for x in y]
+
+    self.colapscals = {self.collaps(x,False):1             for y in self.allCalls      for x in y}
+    self.colapsrefs = {self.collaps(x,True):1              for y in self.allFieldRefs  for x in y}
+
+  def collaps(self, x, isref):
+    f  = self.data['methods'][x['F']-1]
+    fc = self.data['classes'][f['c']-1]
+    t  = self.data['fields'][x['T']-1] if isref else self.data['methods'][x['T']-1] 
+    tc = self.data['classes'][t['c']-1]
+    return (f['q'] if f['g'] else fc['q'], t['q'] if t['g'] else tc['q'], x['s'], x['e'])
 
   def write_debug_csv(self,oup):
-    def mkrec(x,cat,carr,farr,tarr):
-      return list([x['q'],
-                   cat,
-                   x['i'],
-                   x['n'] if 'n' in x else '',
-                   carr[x['c']-1]['n']              if 'c' in x else '',
-                   farr[x['F']-1]['n']              if 'F' in x else '',
-                   carr[farr[x['F']-1]['c']-1]['n'] if 'F' in x else '',
-                   tarr[x['T']-1]['n']              if 'T' in x else '',
-                   carr[tarr[x['T']-1]['c']-1]['n'] if 'T' in x else '',
-                   'static' if 's' in x and x['s'] is True else '',
-                   'external' if x['e'] is True else '',
-                   x['G'] if 'G' in x and x['G'] is not None else ''])
-
     csvw = csv.writer(oup)
     csvw.writerow(['mznid','cat','catid','name','class','felement','fclass','telement','tclass','static','external','label'])
-
-    for y,cat,carr,farr,tarr in [
-      (self.allClasses,   'Class',  self.data['classes'], [], []),
-      (self.allFields,    'Field',  self.data['classes'], [], []),
-      (self.allMethods,   'Method', self.data['classes'], [], []),
-      (self.allCalls,     'Call',   self.data['classes'], self.data['methods'], self.data['methods']),
-      (self.allFieldRefs, 'Access', self.data['classes'], self.data['methods'], self.data['fields']),
-    ]:
-      for z in y:
-        for x in z: csvw.writerow(mkrec(x,cat,carr,farr,tarr))
+    for y,cat,ann,ext,sta in self.allInOrder:
+      carr = self.data['classes']
+      farr = self.data['methods']
+      tarr = self.data['fields'] if cat == 'Access' else self.data['methods']
+      csvw.writerows([
+        [x['q'],
+         cat,
+         x['i'],
+         x['n'] if 'n' in x else '',
+         carr[x['c']-1]['n']              if 'c' in x else '',
+         farr[x['F']-1]['n']              if 'F' in x else '',
+         carr[farr[x['F']-1]['c']-1]['n'] if 'F' in x else '',
+         tarr[x['T']-1]['n']              if 'T' in x else '',
+         carr[tarr[x['T']-1]['c']-1]['n'] if 'T' in x else '',
+         sta,
+         ext,
+         ann,
+         x['G'] if 'G' in x and x['G'] is not None else ''] for x in y])
 
   def write_model_mzn(self,oup):
-    # Calculate variables for MiniZinc, maintaining same order
-    MaxFuncParms                     = self.maxnpmth
-    Annotated_start                  = 1
-    Annotated_end                    = Annotated_start                 + len(self.intan) - 1
-    Unannotated_start                = Annotated_end                   + 1
-    Unannotated_end                  = Unannotated_start               + len(self.intun) - 1
-    External_start                   = Unannotated_end                 + 1
-    External_end                     = External_start                  + len(self.extun) - 1
-    AnnotatedInstanceField_start     = External_end                    + 1
-    AnnotatedInstanceField_end       = AnnotatedInstanceField_start    + len(self.iiafld) - 1
-    UnannotatedInstanceField_start   = AnnotatedInstanceField_end      + 1
-    UnannotatedInstanceField_end     = UnannotatedInstanceField_start  + len(self.iiufld) - 1
-    ExternalInstanceField_start      = UnannotatedInstanceField_end    + 1
-    ExternalInstanceField_end        = ExternalInstanceField_start     + len(self.eiufld) - 1
-    AnnotatedClassField_start        = ExternalInstanceField_end       + 1
-    AnnotatedClassField_end          = AnnotatedClassField_start       + len(self.isafld) - 1
-    UnannotatedClassField_start      = AnnotatedClassField_end         + 1
-    UnannotatedClassField_end        = UnannotatedClassField_start     + len(self.isufld) - 1
-    ExternalClassField_start         = UnannotatedClassField_end       + 1
-    ExternalClassField_end           = ExternalClassField_start        + len(self.esufld) - 1
-    AnnotatedInstanceMethod_start    = ExternalClassField_end          + 1
-    AnnotatedInstanceMethod_end      = AnnotatedInstanceMethod_start   + len(self.iiamth) - 1
-    UnannotatedInstanceMethod_start  = AnnotatedInstanceMethod_end     + 1
-    UnannotatedInstanceMethod_end    = UnannotatedInstanceMethod_start + len(self.iiumth) - 1
-    ExternalInstanceMethod_start     = UnannotatedInstanceMethod_end   + 1
-    ExternalInstanceMethod_end       = ExternalInstanceMethod_start    + len(self.eiumth) - 1
-    AnnotatedClassMethod_start       = ExternalInstanceMethod_end      + 1
-    AnnotatedClassMethod_end         = AnnotatedClassMethod_start      + len(self.isamth) - 1
-    UnannotatedClassMethod_start     = AnnotatedClassMethod_end        + 1
-    UnannotatedClassMethod_end       = UnannotatedClassMethod_start    + len(self.isumth) - 1
-    ExternalClassMethod_start        = UnannotatedClassMethod_end      + 1
-    ExternalClassMethod_end          = ExternalClassMethod_start       + len(self.esumth) - 1
-    InternalInstanceCall_start       = ExternalClassMethod_end         + 1
-    InternalInstanceCall_end         = InternalInstanceCall_start      + len(self.iicll) - 1
-    InternalStaticCall_start         = InternalInstanceCall_end        + 1
-    InternalStaticCall_end           = InternalStaticCall_start        + len(self.iscll) - 1
-    ExternalInstanceCall_start       = InternalStaticCall_end          + 1
-    ExternalInstanceCall_end         = ExternalInstanceCall_start      + len(self.eicll) - 1
-    ExternalStaticCall_start         = ExternalInstanceCall_end        + 1
-    ExternalStaticCall_end           = ExternalStaticCall_start        + len(self.escll) - 1
-    InternalInstanceAccess_start     = ExternalStaticCall_end          + 1
-    InternalInstanceAccess_end       = InternalInstanceAccess_start    + len(self.iiacc) - 1
-    InternalStaticAccess_start       = InternalInstanceAccess_end      + 1
-    InternalStaticAccess_end         = InternalStaticAccess_start      + len(self.isacc) - 1
-    ExternalInstanceAccess_start     = InternalStaticAccess_end        + 1
-    ExternalInstanceAccess_end       = ExternalInstanceAccess_start    + len(self.eiacc) - 1
-    ExternalStaticAccess_start       = ExternalInstanceAccess_end      + 1
-    ExternalStaticAccess_end         = ExternalStaticAccess_start      + len(self.esacc) - 1
-    Class_start                      = Annotated_start
-    Class_end                        = External_end
-    InstanceField_start              = AnnotatedInstanceField_start
-    InstanceField_end                = ExternalInstanceField_end
-    ClassField_start                 = AnnotatedClassField_start
-    ClassField_end                   = ExternalClassField_end
-    Field_start                      = InstanceField_start
-    Field_end                        = ClassField_end
-    InstanceMethod_start             = AnnotatedInstanceMethod_start
-    InstanceMethod_end               = ExternalInstanceMethod_end
-    ClassMethod_start                = AnnotatedClassMethod_start
-    ClassMethod_end                  = ExternalClassMethod_end
-    Method_start                     = InstanceMethod_start
-    Method_end                       = ClassMethod_end
-    Element_start                    = Field_start
-    Element_end                      = Method_end
-    Node_start                       = Class_start
-    Node_end                         = Element_end
-    Call_start                       = InternalInstanceCall_start
-    Call_end                         = ExternalStaticCall_end
-    Access_start                     = InternalInstanceAccess_start
-    Access_end                       = ExternalStaticAccess_end
-    Edge_start                       = Call_start
-    Edge_end                         = Access_end
+    oup.write('%s=%s;\n' % ('MaxFuncParms',self.maxnpmth))
 
-    # Write MiniZinc file
-    for x in [
-      'MaxFuncParms',
-      'Annotated_start',                 'Annotated_end', 
-      'Unannotated_start',               'Unannotated_end',
-      'External_start',                  'External_end',
-      'AnnotatedInstanceField_start',    'AnnotatedInstanceField_end',
-      'UnannotatedInstanceField_start',  'UnannotatedInstanceField_end',
-      'ExternalInstanceField_start',     'ExternalInstanceField_end',
-      'AnnotatedClassField_start',       'AnnotatedClassField_end',
-      'UnannotatedClassField_start',     'UnannotatedClassField_end',
-      'ExternalClassField_start',        'ExternalClassField_end',
-      'AnnotatedInstanceMethod_start',   'AnnotatedInstanceMethod_end',
-      'UnannotatedInstanceMethod_start', 'UnannotatedInstanceMethod_end',
-      'ExternalInstanceMethod_start',    'ExternalInstanceMethod_end',
-      'AnnotatedClassMethod_start',      'AnnotatedClassMethod_end',
-      'UnannotatedClassMethod_start',    'UnannotatedClassMethod_end',
-      'ExternalClassMethod_start',       'ExternalClassMethod_end',
-      'InternalInstanceCall_start',      'InternalInstanceCall_end',
-      'InternalStaticCall_start',        'InternalStaticCall_end',
-      'ExternalInstanceCall_start',      'ExternalInstanceCall_end',
-      'ExternalStaticCall_start',        'ExternalStaticCall_end',
-      'InternalInstanceAccess_start',    'InternalInstanceAccess_end',
-      'InternalStaticAccess_start',      'InternalStaticAccess_end',
-      'ExternalInstanceAccess_start',    'ExternalInstanceAccess_end',
-      'ExternalStaticAccess_start',      'ExternalStaticAccess_end',
-      'Class_start',                     'Class_end',
-      'InstanceField_start',             'InstanceField_end',
-      'ClassField_start',                'ClassField_end',
-      'Field_start',                     'Field_end',
-      'InstanceMethod_start',            'InstanceMethod_end',
-      'ClassMethod_start',               'ClassMethod_end',
-      'Method_start',                    'Method_end',
-      'Element_start',                   'Element_end',
-      'Node_start',                      'Node_end',
-      'Call_start',                      'Call_end',
-      'Access_start',                    'Access_end',
-      'Edge_start',                      'Edge_end'
-    ]:
-      oup.write('%s=%s;\n' % (x,locals()[x]))
+    accum = 0
+    for y0,y1,y2,y3,y4 in self.allInOrder:
+      x = y2 + y3 + y4 + y1 
+      accum =  accum + 1
+      oup.write('%s_start=%s;\n' % (x, accum))
+      accum =  accum + len(y0) - 1
+      oup.write('%s_end=%s;\n'   % (x, accum))
 
     for (x,y,z) in [
-      ('hasClass',   self.hasClass,   'Element'),
-      ('hasCaller',  self.hasCaller,  'Call' ),
-      ('hasCallee',  self.hasCallee,  'Call'),
-      ('fromMethod', self.fromMethod, 'Access'),
-      ('toField',    self.toField,    'Access')
+      ('hasAnnotation', list(map(fix,self.hasAnnotation)), 'AnnotatedElement'),
+      ('hasClass',      self.hasClass,                     'Element'),
+      ('callFrom',      self.callFrom,                     'Call' ),
+      ('callTo',        self.callTo,                       'Call'),
+      ('accessFrom',    self.accessFrom,                   'Access'),
+      ('accessTo',      self.accessTo,                     'Access')
     ]:
       oup.write('%s=array1d(%s,[\n%s\n]);\n' % (x,z,brklns(y, 10)))
-
-    for (x,y) in mdl.userAnnElt:
-      z = fix(y)
-      oup.write('constraint :: "AnnotateOn%s" labelAtLevel[%s,hasLabelLevel[%s]] = %s;\n' % (x,x,z,z))
-      oup.write('constraint :: "ProhibitOn%s" forall (l in Level where l != hasLabelLevel[%s]) (labelAtLevel[%s,l] = nullCleLabel);\n' % (x,z,x))
-
-def fix(x): return x.rsplit('.',1)[-1]
-def annotsplit(j):
-  for x in j:
-    x['cle-label'] = fix(x['cle-label'])
-    if 'cdf' in x['cle-json']:
-      for y in x['cle-json']['cdf']:
-        if 'rettaints' in y:
-          y['rettaints'] = list(map(fix, y['rettaints']))
-          y['codtaints'] = list(map(fix, y['codtaints']))
-          y['argtaints'] = list(map(lambda w: list(map(fix, w)), y['argtaints']))
-  return j
-
-def get_args():
-  p = ArgumentParser(description='Constraint Instance Data Encoder')
-  p.add_argument('-i', '--input_model', required=True, type=str, help='Gzipped Input Program Model JSON file')
-  p.add_argument('-c', '--cle_json', required=True, type=str, help='Input Program Model JSON file')
-  p.add_argument('-o', '--output_dir', required=False, default='./instance', type=str, help='Output Model MZN file')
-  return p.parse_args()
 
 if __name__ == '__main__':
   logger = logging.getLogger()
   logger.addHandler(logging.StreamHandler(sys.stderr))
   logger.setLevel(logging.WARN)
   args   = get_args()
+  print('Loading model ...')
   with gzip.open(args.input_model, 'rt') as minp:
     mdl  = Model(esatrack(json.load(minp)))
+  print('Loaded model')
   with open(args.cle_json, 'r') as cinp:
     cmz = compute_zinc(annotsplit(json.load(cinp)), mdl.maxnpmth, logger)
+  print('Loaded cle')
+  print('Writing data instances ...')
   if not os.path.exists(args.output_dir):
     os.makedirs(args.output_dir)
   with open(args.output_dir + '/pdg_instance.mzn', 'w') as moup:
@@ -325,6 +238,9 @@ if __name__ == '__main__':
     coup.write(cmz.cle_instance)
   with open(args.output_dir + '/enclave_instance.mzn', 'w') as eoup:
     eoup.write(cmz.enclave_instance)
+  print('Wrote data instances')
+  print('Writing debug file ...')
   with gzip.open(args.output_dir + '/pdg.csv.gz', 'wt') as csvp:
     mdl.write_debug_csv(csvp)
+  print('Wrote debug file')
 
