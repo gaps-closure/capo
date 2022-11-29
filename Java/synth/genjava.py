96e8e4133a16e9cd3e1dba3efae1c33c8cc863a3
Rajesh Krishnan <rkrishnan@perspectalabs.com>
2022-11-29 17:46:16 -0500

    move spec to separate file

--- Java/synth/genjava.py
+++ Java/synth/genjava.py
@@ -1,108 +1,126 @@
 #!/usr/bin/python3
 import json
-import random
 import math
+import time
+import random       as r
 import numpy.random as nr
+from   dataclasses  import dataclass
 
-pkgprefix      = "com.peratonlabs.jgen"
-prim_fields    = ["byte", "short", "int", "long", "float", "double", "boolean", "char", "String"]
+@dataclass
+class JGenSpec:
+  pkgprefix:     str       # Package prefix to use
+  rseed:         int       # Seed for random generator
+  primtypes:     [str]     # primitive data types plus string
+  fprim_freq:    [float]   # Probability vector of field types for elements in primtypes 
+  pprim_freq:    [float]   # Probability vector of method parameter types for elements in primtypes
+  rprim_freq:    [float]   # Probability vector of method return types for elements in primtypes
+  class_a_mods:  [str]     # Access modifiers for class
+  class_a_freq:  [float]   # Probability vector for access modifiers for class 
+  field_a_mods:  [str]     # Access modifiers for fields
+  field_a_freq:  [float]   # Probability vector for access modifiers for field
+  method_a_mods: [str]     # Access modifiers for methods
+  method_a_freq: [float]   # Probability vector for access modifiers for method 
+  mprob:         float     # Probability that class element is a method vs. field
+  farrayprob:    float     # Probability that a field is an array (vs. scalar)
+  fclassprob:    float     # Probability that a field is a class (vs. primitive type or string)
+  parrayprob:    float     # Probability that a parameter is an array (vs. scalar)
+  pclassprob:    float     # Probability that a parameter is a class (vs. primitive or string)
+  rarrayprob:    float     # Probability that a return type is an array (vs. scalar)
+  rclassprob:    float     # Probability that a return type is a class (vs. primitive or string)
+  meanparms:     float     # Mean number of parameters in a method
+  maxparms:      float     # Maximum number of parameters in a method
+  meanelts:      float     # Mean number of elements in a base class 
+  maxelts:       float     # Maximum number of elements in a base class 
+  fubars:        [str]     # Names of formal parameters
+  nouns:         [str]     # Nouns to choose from for class and field names
+  adjectives:    [str]     # Adjectives to choose from for class names
+  verbs:         [str]     # Verbs to choose from for method names
+  adverbs:       [str]     # Adverbs to choose from for method names
 
-mprob          = 0.2
-fprim_freq     = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2]
-pprim_freq     = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2]
-rprim_freq     = [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.2]
+  def set_rseed(self, use_time=False): 
+    s = int(time.time()) if use_time else self.rseed
+    nr.seed(s)
+    r.seed(s)
 
-farrayprob     = 0.1
-fclassprob     = 0.2
+  def update(self, w):
+    for k,v in w.items():
+      if hasattr(self, k): setattr(self, k, v)
 
-parrayprob     = 0.1
-pclassprob     = 0.2
-
-rarrayprob     = 0.1
-rclassprob     = 0.2
-
-foverrideprob  = 0.0
-
-moverrideprob  = 0.2
-moverloadprob  = 0.2
-
-meanparms      = 4.0
-maxparms       = 20.0
-
-meanelts       = 8.0
-maxelts        = 20.0
-
-class_a_mods   = ["public", ""]
-field_a_mods   = ["public", "private", "protected", ""]
-method_a_mods  = ["public", "private", "protected", ""]
-class_na_mods  = ["final", "abstract", ""]
-method_na_mods = ["final", "static", "abstract", "transient", "synchronized", ""]
-field_na_mods  = ["final", "static", "transient", "volatile", ""]
-
-class_a_freq   = [0.1, 0.9]
-method_a_freq  = [0.2, 0.4, 0.4, 0.0]
-field_a_freq   = [0.05, 0.65, 0.3, 0.0]
-class_na_freq  = [0.1, 0.1, 0.8]
-method_na_freq = [0.05, 0.05, 0.0, 0.0, 0.0, 0.9]
-field_na_freq  = [0.05, 0.05, 0.0, 0.0, 0.9]
-
-words = {}
-
-def pick_name(pl): 
-  return ''.join(list(map(lambda x: random.choice(words[x]) if x=="verbs" else random.choice(words[x]).capitalize(), pl)))
+def pick(wl,capitalize=True): 
+  return r.choice(wl).capitalize() if capitalize else r.choice(wl)
 
 def pick_type(primflds, allclasses, primfreq, classprob, arrayprob):
   assert(classprob >= 0.0 and classprob <= 1.0)
   assert(arrayprob >= 0.0 and arrayprob <= 1.0)
   assert(len(primflds) == len(primfreq))
   assert(sum(primfreq) == 1.0)
-  ft = random.choice(allclasses) if random.random() < classprob else nr.choice(primflds, 1, p = primfreq)[0]
-  ar = "[]" if random.random() < arrayprob else ""
+  assert(len(primflds) > 0)
+  ft = r.choice(allclasses) if r.random() < classprob else nr.choice(primflds, 1, p = primfreq)[0]
+  ar = "[]" if r.random() < arrayprob else ""
   return ft + ar
-  
-def pick_modif():  return ""
-def pick_class_modif():  return ""
-def pick_field_modif():  return ""
-def pick_method_modif():  return ""
 
-def pick_class_name():   return pick_name(["adjectives", "nouns"])
-def pick_subcl_name(cn): return pick_name(["adjectives"]) + cn
-def pick_field_name():   return "f" + pick_name(["nouns"])
-def pick_method_name():  return pick_name(["verbs","adverbs"])
+def pick_access(mods, modfreq):
+  assert(len(mods) == len(modfreq))
+  assert(sum(modfreq) == 1.0)
+  assert(len(mods) > 0)
+  return nr.choice(mods, 1, modfreq)[0]
 
-def pick_field_type():  return pick_type(prim_fields, ["ClassTBD"], fprim_freq, fclassprob, farrayprob)
-def pick_return_type(): return pick_type(prim_fields, ["ClassTBD"], rprim_freq, rclassprob, rarrayprob)
-def pick_param_type():  return pick_type(prim_fields, ["ClassTBD"], pprim_freq, pclassprob, parrayprob)
+def pick_field_name(j):    return "f" + pick(j.nouns)
+def pick_subcl_name(j,cn): return pick(j.adjectives) + cn
+def pick_method_name(j):   return pick(j.verbs,False) + pick(j.adverbs)
+def pick_class_name(j):    return pick(j.adjectives) + pick(j.nouns)
+def pick_field_type(j):    return pick_type(j.primtypes, ["ClassTBD"], j.fprim_freq, j.fclassprob, j.farrayprob)
+def pick_return_type(j):   return pick_type(j.primtypes, ["ClassTBD"], j.rprim_freq, j.rclassprob, j.rarrayprob)
+def pick_param_type(j):    return pick_type(j.primtypes, ["ClassTBD"], j.pprim_freq, j.pclassprob, j.parrayprob)
+def pick_class_access(j):  return pick_access(j.class_a_mods, j.class_a_freq)
+def pick_field_access(j):  return pick_access(j.field_a_mods, j.field_a_freq)
+def pick_method_access(j): return pick_access(j.method_a_mods, j.method_a_freq)
+
+"""
+foverrideprob  = 0.0
+moverrideprob  = 0.2
+moverloadprob  = 0.2
+class_na_mods  = ["final", "abstract", ""]
+method_na_mods = ["final", "static", "abstract", "transient", "synchronized", ""]
+field_na_mods  = ["final", "static", "transient", "volatile", ""]
+"""
 
 if __name__ == '__main__':
-  with open('vocab.json', 'r') as vj:
-    words = json.load(vj)
+  with open("gspec.json", 'r') as f: j = JGenSpec(**(json.load(f)))
+  with open("vocab.json", 'r') as f: j.update(json.load(f))
+  j.set_rseed(use_time=True)
 
-  cn = pick_class_name()
+  # XXX: must construct class hierarchy first
+ 
+  ca = pick_class_access(j)
+  cn = pick_class_name(j)
   # check for duplicates, decide inheritance
-  print("class " + cn)
+  print((ca if ca=="" else ca + " ") + "class " + cn + " implements Serializable {")
 
-  nelts = int(math.floor(min(random.expovariate(1.0) * meanelts, maxelts)))
+  nelts = int(math.floor(min(r.expovariate(1.0) * j.meanelts, j.maxelts)))
 
   for i in range(nelts):
-    if random.random() < mprob: 
-      rt = pick_return_type()
-      mn = pick_method_name()  # check dups, overriding
-      nparms = int(math.floor(min(random.expovariate(1.0) * meanparms, maxparms)))
+    if r.random() < j.mprob: 
+      ma = pick_method_access(j)
+      rt = pick_return_type(j)
+      mn = pick_method_name(j)  # check dups, overriding
+      nparms = int(math.floor(min(r.expovariate(1.0) * j.meanparms, j.maxparms)))
       pstr = ""
       for p in range(nparms):
-        pn = words["fubars"][p]
-        pt = pick_param_type()
+        pn = j.fubars[p]
+        pt = pick_param_type(j)
         pstr += pt
         pstr += " "
         pstr += pn
         pstr += ", " if p < nparms - 1 else ""
         
-      print(rt + " " + mn + "(" + pstr + ");")
+      print("  " + (ma if ma=="" else ma + " ") + rt + " " + mn + "(" + pstr + ");")
 
     else:
-      ft = pick_field_type()
-      fn = pick_field_name()
+      fa = pick_field_access(j)
+      ft = pick_field_type(j)
+      fn = pick_field_name(j)
       # check for duplicates and parent field overrides
-      print(ft + " " + fn + ";")
+      print("  " + (fa if fa=="" else fa + " ") + ft + " " + fn + ";")
 
+  print("};")
