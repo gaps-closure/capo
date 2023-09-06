14b0dd77436e5473c00be8b96cc1ea760edad10f
Max Levatich <ml4553@columbia.edu>
2023-09-06 13:44:35 -0400

    bugfixes

--- C/conflict_analyzer/pdg2zinc.py
+++ C/conflict_analyzer/pdg2zinc.py
@@ -13,8 +13,8 @@ def pdg_to_zinc(pdg_csv, max_fn_parms):
         ("Annotation", ["Var", "Global", "Other"]),
         ("ControlDep", ["CallInv", "Indirect_CallInv", "CallRet", "Entry", "Br", "Other"]),
         ("DataDepEdge", ["DefUse", "RAW", "Ret", "Indirect_Ret", "Alias", 
-                         "Argpass_In", "Argpass_Out", "Argpass_Indirect_In", 
-                         "Argpass_Indirect_Out", "Callee"]),
+                         "ArgPass_In", "ArgPass_Out", "ArgPass_Indirect_In", 
+                         "ArgPass_Indirect_Out", "Callee"]),
         ("Parameter", ["In", "Out", "Field"]),
         ("Anno", ["Global", "Var", "Other"]),
         (None, ["DataDepEdge_PointsTo"])
